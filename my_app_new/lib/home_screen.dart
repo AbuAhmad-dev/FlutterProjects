5c816befd651fe0ede0b57bd180cce1d3ecf2554
AbuAhmad-dev <basebata@gmail.com>
2025-12-25 01:54:38 +0300

    Implement HomeScreen layout with AppBar and welcome message

--- my_app_new/lib/home_screen.dart
+++ my_app_new/lib/home_screen.dart
@@ -5,6 +5,17 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('flutter_developer.com'),
+      ),
+      body: const Center(
+        child: Text(
+          'Welcome to the Home Screen!',
+          style: TextStyle(fontSize: 24),
+        ),
+      ),
+    );
+      
   }
 }
