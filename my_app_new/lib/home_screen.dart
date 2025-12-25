86b9f18b288d862cc5fbf3d0d4149e63b96b66b0
AbuAhmad-dev <basebata@gmail.com>
2025-12-25 07:39:17 +0300

    commit

--- my_app_new/lib/home_screen.dart
+++ my_app_new/lib/home_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -8,13 +10,46 @@ class HomeScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text('flutter_developer.com'),
+        centerTitle: true,
+        actions: [Icon(Icons.developer_board_outlined)],
+        leading: Icon(Icons.menu),
+        backgroundColor: const Color.fromARGB(255, 255, 174, 68),
       ),
-      body: const Center(
-        child: Text(
-          'Welcome to the Home Screen!',
-          style: TextStyle(fontSize: 24),
-        ),
-      ),
+      backgroundColor: const Color.fromARGB(255, 196, 218, 233),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        spacing: 20,
+        verticalDirection:VerticalDirection.down, 
+        children: [
+          Text(
+            ('Welcome to the Home Screen!'),
+            style: TextStyle(fontSize: 24,
+            fontWeight: FontWeight.bold
+           ),),
+          Text("This is a simple Flutter application.",
+          style: TextStyle(fontSize: 18),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Icon(Icons.home, size: 50, color: Colors.blue),
+              Icon(Icons.person, size: 50, color: Colors.green),
+              Icon(Icons.settings, size: 50, color: Colors.orange),
+            ],
+          ),
+          FilledButton(onPressed: () {
+            log("welcome to my application");
+          },
+           child: Text("start from here")),
+          ElevatedButton(
+            style: ElevatedButton.styleFrom(backgroundColor: Colors.red),
+            onPressed: () {
+           log("explore more features");
+        },
+child: Text("explore more")),
+        ],
+      )
     );
       
   }
